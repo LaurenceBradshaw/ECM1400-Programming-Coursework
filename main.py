4ba0a170eb83ba09fa4c2009ab7c9bc2b6169b4c
Lozing <lozingyt@gmail.com>
2022-11-05 00:51:17 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+# This is a template. 
+# You should modify the functions below to match
+# the signatures determined by the project specification
+
+
+def main_menu():
+    """Your documentation goes here"""
+    valid_options = ['r', 'i', 'm', 'a', 'q']
+
+
+def monitoring_menu():
+    """Your documentation goes here"""
+    # Your code goes here
+
+
+def intelligence_menu():
+    """Your documentation goes here"""
+    # Your code goes here
+
+
+def about():
+    """Your documentation goes here"""
+    # Your code goes here
+
+
+def quit():
+    """Your documentation goes here"""
+    # Your code goes here
+
+
+if __name__ == '__main__':
+    main_menu()
+
+# Questions:
+# Can we make other functions that are not specified in the template? - does it matter what file we put them in?
+# Can we import other libraries? Such as the regex library
+# Why do you need both data and monitoring_station parameters in the functions in reporting.py? The way I see it is that you can read the data with the station name or just pass the data in, so why both?
+#
+
---
+++ utils.py
@@ -0,0 +1,23 @@
+# This is a template. 
+# You should modify the functions below to match
+# the signatures determined by the project specification
+
+
+def sumvalues(values):
+    """Your documentation goes here"""
+
+
+def maxvalue(values):
+    """Your documentation goes here"""
+
+
+def minvalue(values):
+    """Your documentation goes here"""
+
+
+def meannvalue(values):
+    """Your documentation goes here"""
+
+
+def countvalue(values,xw):
+    """Your documentation goes here"""
